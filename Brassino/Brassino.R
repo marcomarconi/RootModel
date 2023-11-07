d53d1c5cadcacd1fcd63d655bd4a92a5fe4ff2fe
marco.marconi <marco.marconi@gmail.com>
2023-11-07 22:18:31 +0100

    o

--- Brassino/Brassino.R
+++ Brassino/Brassino.R
@@ -6,7 +6,7 @@ theme_set(theme_bw())
 #for f in `ls output*_`; do grep "Root length [^ ]* Meristem length [^ ]* Time [0-9]*" $f  -o | sed 's/Root length \([0-9\.]*\) Meristem length \([0-9\.]*\) Time \([0-9]*\)/\3,\1,\2/' | uniq | grep -v nan > $f.out; done
 
 
-dir <- "/home/marco/trabajo/Models/RootModel/Brassino/Brassino_Pin_4_eponly/"
+dir <- "/home/marco/trabajo/Models/RootModel/Brassino/Brassino_Pin_6_allcells///"
 setwd(dir)
 files <- list()
 for(f in list.files(".","*.out")) {
@@ -17,9 +17,9 @@ for(f in list.files(".","*.out")) {
   df$Delay <- values[3]
   df$Auxin <- values[4]  %>% factor()
   colnames(df) <- c("Time", "Root", "Meristem", "PIN", "Type", "Delay", "Auxin")
-  df$GR <- EMA(c(0, diff((df$Root))), 5)
+  df$GR <- EMA(c(0, diff((df$Root))), 20)
   files[[f]] <- df
 }
 
 df <- do.call(rbind, files)
-ggplot(df %>% filter(PIN=="1.0" & Delay=="20" & Auxin=="100"  )) + geom_line(aes(Time, GR, color=Type), linewidth=3) + scale_color_colorblind()
+ggplot(df %>% filter(PIN=="0.1" & Delay=="20" & Auxin=="100" & Time > 200 & Time <600 )) + geom_line(aes(Time, GR, color=Type), linewidth=3) + scale_color_colorblind() + theme(text=element_text(size=48))
