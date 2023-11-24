15ed39ef02b99d642f50ed07cbad79b33bfd139d
marco.marconi <marco.marconi@gmail.com>
2023-11-24 12:32:41 +0100

    o

--- Brassino/Brassino.R
+++ Brassino/Brassino.R
@@ -6,23 +6,24 @@ theme_set(theme_bw())
 #for f in `ls output*_`; do grep "Root length [^ ]* Meristem length [^ ]* Time [0-9]*" $f  -o | sed 's/Root length \([0-9\.]*\) Meristem length \([0-9\.]*\) Time \([0-9]*\)/\3,\1,\2/' | uniq | grep -v nan > $f.out; done
 
 
-dir <- "/home/marco/trabajo/Models/RootModel/Brassino/Brassino_Pin_6_allcells///"
+dir <- "/home/marco/trabajo/Models/RootModel/Brassino/Brassino_New//"
 setwd(dir)
 files <- list()
 for(f in list.files(".","*.out")) {
   df <- read_csv(f, show_col_types = FALSE, col_names = FALSE) 
   values <- sub("output.", "", f) %>% sub("_.out", "", .) %>% gsub("_", " ",.) %>% strsplit(., " ") %>% unlist
-  df$PIN <- values[1] %>% factor()
   df$Type <- values[2] %>% factor()
   df$Delay <- values[3]
-  df$Auxin <- values[4]  %>% factor()
-  colnames(df) <- c("Time", "Root", "Meristem", "PIN", "Type", "Delay", "Auxin")
+  colnames(df) <- c("Time", "Root", "Meristem", "Type", "Delay")
   df$GR <- EMA(c(0, diff((df$Root))), 20)
   files[[f]] <- df
 }
 
 df <- do.call(rbind, files)
-ggplot(df %>% filter(PIN=="0.1" & Delay=="20" & Auxin=="100" & Time > 200 & Time <600 )) + geom_line(aes(Time, GR, color=Type), linewidth=3) + scale_color_colorblind() + theme(text=element_text(size=48))
+ggplot(df %>% filter(Time > 0)) + geom_line(aes(Time, GR, color=Type), linewidth=3) + scale_color_colorblind() + 
+    theme(text=element_text(size=48)) + facet_wrap(~Delay)
+
+
 
 {
 dir <- "/home/marco/trabajo/Models/RootModel/"
@@ -39,3 +40,11 @@ group_by(df, Type, Time=ceiling(Time), Top) %>% filter(Type>5 & !between(Type, 1
   b <- scan("_b")
   plot.ts(diff(b))
 }
+
+
+system("grep Bras _out > _a")
+a <- read_csv("_a", col_names=F) %>% mutate(Type=factor(X5), X=ceiling(X4)) %>% filter(!X2 %in% c(0:5, 10:11, 14:15))
+z <- group_by(a, Type, X, X2) %>% summarize(Y=median(X6), SD=2*mad(X6)/sqrt(n()), N=n())
+ggplot(z %>% filter(X<20)) + geom_line(aes(X, Y, col=Type), linewidth=2) + 
+    geom_errorbar(aes(x=X, ymin=Y-SD, ymax=Y+SD), width=0.5, color="gray")+ facet_wrap(~X2) #+ ylim(c(0,0.03))
+
