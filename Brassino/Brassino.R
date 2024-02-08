dfac11ae0afc5647cc0c0c5c621a973a8472742d
marco.marconi <marco.marconi@gmail.com>
2024-02-08 22:06:30 +0100

    o

--- Brassino/Brassino.R
+++ Brassino/Brassino.R
@@ -1,17 +1,22 @@
 library(tidyverse)
 library(TTR)
 library(ggthemes)
+library(LaplacesDemon)
 theme_set(theme_bw())
 
 #for f in `ls output*_`; do grep "Root length [^ ]* Meristem length [^ ]* Time [0-9]*" $f  -o | sed 's/Root length \([0-9\.]*\) Meristem length \([0-9\.]*\) Time \([0-9]*\)/\3,\1,\2/' | uniq | grep -v nan > $f.out; done
 
 # Growth rate
 {
-dir <- "/home/marco/trabajo/Models/RootModel/Brassino/Brassino_Auxin_Basal_2h_30steps/"
+dir <- "/home/marco/trabajo/Models/RootModel/Brassino/Brassino_noAuxin_lowEK_2h_10steps//"
 setwd(dir)
 files <- list()
 for(f in list.files(".","*.out")) {
   df <- read_csv(f, show_col_types = FALSE, col_names = FALSE) 
+  if(nrow(df)==0) {
+      warning(paste("No lines in", f))
+      next
+  }
   values <- sub("output.", "", f) %>% sub("_.out", "", .) %>% gsub("_", " ",.) %>% strsplit(., " ") %>% unlist
   df$Basal <- values[1] %>% factor()
   df$Type <- values[2] %>% factor()
@@ -25,10 +30,10 @@ df <- do.call(rbind, files) %>% mutate(Delay = fct_reorder(Delay, as.numeric(Del
 levels(df$Type) <- c("Scen. 1", "Scen. 3-LO", "Scen. 2", "Scen. 3-UP"); df$Type <- factor(df$Type, c("Scen. 1", "Scen. 2", "Scen. 3-UP", "Scen. 3-LO")); 
 levels(df$Delay) <- c("Delay 0", "Delay 20min", "Delay 1.5h", "Delay 3h", "Delay 6h");
 df$Time <- df$Time * 0.03
-ggplot(df %>% filter(Time > 0 & Basal == "0.01" & WGR == "1.0" & Delay!="Delay 0")) + 
+ggplot(df %>% filter(Time < 40 & Basal == "0.01" & WGR == "1.0" & Delay!="Delay 0")) + 
     geom_line(aes(Time, GR, color=Type), linewidth=3) + 
     scale_color_manual(values = c("#014d64", "orange3", "#6794a7", "#01a2d9")) + 
-    theme(text=element_text(size=28), legend.title = element_blank(), legend.text = element_text(size = 18), legend.key.width = unit(x = 1, units = "cm")) + facet_wrap(~Delay) + 
+    theme(text=element_text(size=28), axis.text = element_text(size=18), legend.title = element_blank(), legend.text = element_text(size = 18), legend.key.width = unit(x = 1, units = "cm")) + facet_wrap(~Delay) + 
     ylim(c(0.5, 1.6)) + ylab("Root Growth Rate")+ xlab("Time (h)")
 
 #ggsave("~/GR.pdf", width = 12, height = 9)
@@ -52,13 +57,12 @@ for(f in list.files(".","*.div")) {
   files[[f]] <- df 
 }
     
-df <- do.call(rbind, files) %>% filter(Time < 900 & Time > 300)%>% mutate(Delay = fct_reorder(Delay, as.numeric(Delay))) %>%     group_by(Type, Delay, Basal, WGR) %>% summarize(Count=n())
     levels(df$Type) <- c("Scen. 1", "Scen. 3-LO", "Scen. 2", "Scen. 3-UP"); df$Type <- factor(df$Type, c("Scen. 1", "Scen. 2", "Scen. 3-UP", "Scen. 3-LO")); 
-levels(df$Delay) <- c("Delay 0", "Delay 20min", "Delay 1.5h", "Delay 3h", "Delay 6h");
+levels(df$Delay) <- c("0", "20min", "1.5h", "3h", "6h");
 
-ggplot(df %>% filter(  Basal == "0.01" & WGR == "1.0" & Delay != "Delay 0")) + 
+ggplot(df %>% filter(  Basal == "0.01" & WGR == "1.0" & Delay != "0")) + 
         geom_bar(aes(Delay, Count, fill=Type), color="gray1", stat="identity", position = position_dodge()) +    
-        theme(text=element_text(size=28), axis.text.x = element_text(size=20, angle = 45, vjust = 0.5), axis.title.y = element_text(size=32),legend.text = element_text(size = 18), legend.title = element_blank()) + 
+        theme(text=element_text(size=28), axis.text.x = element_text(size=20, angle = 45, vjust = 0.5),legend.text = element_text(size = 18), legend.title = element_blank()) + 
             xlab("")+  ylab("Cell Divisions")+   scale_fill_manual(values = c("#014d64", "orange3", "#6794a7", "#01a2d9")) 
 
 }
@@ -78,10 +82,10 @@ ggplot(df %>% filter(  Basal == "0.01" & WGR == "1.0" & Delay != "Delay 0")) +
     curve(x*0+1 + 0.03, -100, 10, col="red", lwd=5, lty=2, xaxt = "n",yaxt = "n", xlab="", ylab="", add = T); 
 }    
 {
-    curve(x*0-1, -100, 10, col="blue", lwd=5, xaxt = "n",yaxt = "n", xlab="", ylab="",bty="n", ylim=c(1,-1)); 
-    curve(invlogit((x+50)*10) * invlogit((-x-30)*0.5) * -1 + 0.03, -100, 10, col="blue", lwd=5, lty=2, xaxt = "n",yaxt = "n", xlab="", ylab="",bty="n", add=T); 
+    curve(-invlogit((x+50)*10) * invlogit((-x-47)*2) * -1 -1, -100, 10, col="blue", lwd=5, xaxt = "n",yaxt = "n", xlab="", ylab="",bty="n", ylim=c(1,-1)); 
+    curve((invlogit((x+45)*1) * invlogit((x+30)*-1))*-1 + 0.03, -100, 10, col="red", lwd=5, lty=2, xaxt = "n",yaxt = "n", xlab="", ylab="", add = T);
     curve(x*0 , -100, 10, col="red", lwd=5, xaxt = "n",yaxt = "n", xlab="", ylab="", add = T); 
-    curve(invlogit((x+45)*1) * invlogit((x+30)*-1) - 1 + 0.03, -100, 10, col="red", lwd=5, lty=2, xaxt = "n",yaxt = "n", xlab="", ylab="", add = T); 
+    curve(invlogit((x+50)*10) * invlogit((-x-30)*0.5) * 1 + 0.03 - 1, -100, 10, col="blue", lwd=5, lty=2, xaxt = "n",yaxt = "n", xlab="", ylab="",bty="n", add=T); 
     
 }   
 
