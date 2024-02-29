3605ee008d49cbc52ac99e8f9dd4be9ea04faacd
marco.marconi@gmail.com <marco.marconi@gmail.com>
2024-02-29 16:06:52 +0100

    o

--- WOX5/WOX5.R
+++ WOX5/WOX5.R
@@ -1,6 +1,11 @@
 library(tidyverse)
 theme_set(theme_bw())
 
+# Do:
+# grep -c "type QC" output.*
+# the first two values (auxin source and WOX5 basal production) are the replicate, the second OE/WT/mutant the third the division (divide them by 2)
+# put these 3 columns into a CSV file
+
 df <- read_csv("~/trabajo/Models/RootModel/WOX5/WOX5.csv")
 df$Type <- factor(df$Type, levels=c("WT", "wox5", "OE"))
 group_by(df, Type) %>% summarise(Mean=sum(Divisions)/(3*n()), SD=sd(Divisions)*2/sqrt(n()))
