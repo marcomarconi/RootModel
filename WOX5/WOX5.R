d612e2ae45e07a3896f6f4f54705ae10ac93d0d6
marco.marconi <marco.marconi@gmail.com>
2024-03-04 15:37:56 +0100

    o

--- WOX5/WOX5.R
+++ WOX5/WOX5.R
@@ -12,4 +12,10 @@ group_by(df, Type) %>% summarise(Mean=sum(Divisions)/(3*n()), SD=sd(Divisions)*2
 group_by(df, Type) %>% summarise(Mean=sum(Divisions)/(3*n()), SD=sd(Divisions)/3/sqrt(n())*2) %>% ggplot() +
   geom_bar(aes(Type, Mean), stat="identity") + geom_errorbar(aes(Type, ymin=Mean-SD, ymax=Mean+SD), width=0.25) +
   theme(axis.title.x = element_blank()) + ylab("QC Division (%)")
-                                 
+
+df <- read_csv("~/trabajo/Models/RootModel/WOX5/WOX5_2.csv")
+df$Type <- factor(df$Type, levels=c("WT", "wox5", "OE"))
+group_by(df, Type, Induction) %>% summarise(Mean=sum(Divisions)/(3*n()), SD=sd(Divisions)*2/sqrt(n()))
+group_by(df, Type) %>% summarise(Mean=sum(Divisions)/(3*n()), SD=sd(Divisions)/3/sqrt(n())*2) %>% ggplot() +
+    geom_bar(aes(Type, Mean), stat="identity") + geom_errorbar(aes(Type, ymin=Mean-SD, ymax=Mean+SD), width=0.25) +
+    theme(axis.title.x = element_blank()) + ylab("QC Division (%)")
